1df7f63af4520fda57304c14081c8b48ea838538
Ritik <ritikhooda65@gmail.com>
2022-04-14 02:23:14 +0530

    Updates

---
+++ precache-manifest.352515e1bcce456dec7cdf5dbdf9a7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262353c0715a2cb46f7c1cd27756151c",
+    "url": "/path-finder/index.html"
+  },
+  {
+    "revision": "fd3c29e3ed930d62f613",
+    "url": "/path-finder/static/css/main.b62745c6.chunk.css"
+  },
+  {
+    "revision": "0d5bbf2d86e44c336e49",
+    "url": "/path-finder/static/js/2.202a6893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/path-finder/static/js/2.202a6893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3c29e3ed930d62f613",
+    "url": "/path-finder/static/js/main.2ee84c19.chunk.js"
+  },
+  {
+    "revision": "9731fb23ea4b3728a808",
+    "url": "/path-finder/static/js/runtime-main.43a30262.js"
+  }
+]);
